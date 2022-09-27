aceb7084e2eb0c839596d39c9b2e996f7217ca92
ku6kaw <reoreo01.123@gmail.com>
2022-09-27 15:51:02 +0900

    edit migration

--- database/migrations/2022_09_03_061136_create_tweets_table.php
+++ database/migrations/2022_09_03_061136_create_tweets_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->id();
             $table->string('tweet');
             $table->text('description')->nullable();
-            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
